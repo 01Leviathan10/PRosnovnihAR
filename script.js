d0c58d7b31cfde8d4701df35e50436eba0fb2778
01Leviathan10 <francuz.milicevic@gmail.com>
2021-02-22 22:58:16 +0100

    adding parsFloat

--- script.js
+++ script.js
@@ -2,5 +2,5 @@ const PI = 3.14;
 let poluprecnik = prompt("Molima vas unestipe poluprecnik (r): ");
 let visina = prompt("Molim vas unesite visinu (H): ");
 
-let zapreminaValjka = poluprecnik*2*PI*visina;
+let zapreminaValjka = parseFloat(poluprecnik)*2*PI*parseFloat(visina);
 alert("Zapremina valjka je: "+zapreminaValjka);
