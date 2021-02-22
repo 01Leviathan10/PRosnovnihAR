d51ab060f3dfc85746eb0efed4f4beeda4565d29
01Leviathan10 <francuz.milicevic@gmail.com>
2021-02-22 17:04:50 +0100

    first commit

---
+++ script.js
@@ -0,0 +1,6 @@
+const PI = 3.14;
+let poluprecnik = prompt("Molima vas unestipe poluprecnik (r): ");
+let visina = prompt("Molim vas unesite visinu (H): ");
+
+let zapreminaValjka = poluprecnik*2*PI*visina;
+alert("Zapremina valjka je: "+zapreminaValjka);
